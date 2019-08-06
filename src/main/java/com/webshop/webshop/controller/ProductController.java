90c7024d5d64b61aece49a07606b254f2a2b5c09
Alex Ormandy <120n@jdwilliamsgroup.com>
2019-08-06 13:44:59 +0100

    tuesday afternoon

--- src/main/java/com/webshop/webshop/controller/ProductController.java
+++ src/main/java/com/webshop/webshop/controller/ProductController.java
@@ -15,10 +15,26 @@ public class ProductController {
     public String getProductsAllPage (Model model) {
 
         ProductModel product1 = new ProductModel("Rubber Duck", "Yellow Duck", 12D, "http://google.com");
+        ProductModel product2 = new ProductModel("Football", "Red Ball", 20D, "http://google.com");
 
         List<ProductModel> productModelList = new ArrayList<>();
         productModelList.add(product1);
-//        System.out.println(product1.toString());
+        productModelList.add(product2);
+
+        model.addAttribute("productModelList", productModelList);
+
+        return "productsAll";
+    }
+
+    @GetMapping(value = "/products/{id}")
+    public String getProductsByIDPage (Model model) {
+
+        ProductModel product1 = new ProductModel("Rubber Duck", "Yellow Duck", 12D, "http://google.com");
+        ProductModel product2 = new ProductModel("Football", "Red Ball", 20D, "http://google.com");
+
+        List<ProductModel> productModelList = new ArrayList<>();
+        productModelList.add(product1);
+        productModelList.add(product2);
 
         model.addAttribute("productModelList", productModelList);
 
