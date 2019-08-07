cc47431e14f874d65311c6a776e2a044d9325c41
Alex Ormandy <120n@jdwilliamsgroup.com>
2019-08-07 13:39:42 +0100

    wednesday afternoon

--- src/main/java/com/webshop/webshop/WebshopApplication.java
+++ src/main/java/com/webshop/webshop/WebshopApplication.java
@@ -21,11 +21,16 @@ public class WebshopApplication {
 
 		productDAO.clearRepository();
 
-		ProductModel product1 = new ProductModel("Rubber Duck", "Yellow Duck", 12D, "http://google.com");
-		ProductModel product2 = new ProductModel("Football", "Red Ball", 20D, "http://google.com");
+		ProductModel product1 = new ProductModel("Navy Zak Puppytooth Suit Jacket", "Navy puppytooth suit jacket in a regular fit with a two button fasten, " +
+				"jetted chest pocket with detachable pocket square", 12D, "/r08yy131704w.jpg");
+		ProductModel product2 = new ProductModel("Blue Short Sleeve Textured Double Collar Shirt Long", "Stay on trend this season with our short sleeve double " +
+				"collar button down shirt with a textured print design.", 20D, "/r08ca136705w.jpg");
+		ProductModel product3 = new ProductModel("Vans Ward Trainers", "The Ward trainers feature the iconic Vans classic side stripe, padded collars for comfort " +
+				"and platform rubber waffle outsoles for a firm grip.", 20D, "/q08yd333707s.jpg");
 
 		productDAO.saveProduct(product1);
 		productDAO.saveProduct(product2);
+		productDAO.saveProduct(product3);
 	}
 
 }
