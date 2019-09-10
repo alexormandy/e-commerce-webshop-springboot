3eddf0b9038d78b84170c3591a8c617e2a08ab4b
Alex Ormandy <120n@jdwilliamsgroup.com>
2019-09-10 11:22:37 +0100

    done?

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
 REPLACE INTO `customer_roles` VALUES (true,'CUSTOMER');
 
-INSERT INTO product (id, name, description, price, picture_url) VALUES (67, 'Test Item', "Test Description", 20.95, "/r08yp852764w.jpg");
+--INSERT INTO product (id, name, description, price, picture_url) VALUES (67, 'Test Item', "Test Description", 20.95, "/r08yp852764w.jpg");
